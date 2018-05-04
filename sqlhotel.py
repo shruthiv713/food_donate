1d00b75bc20961ee01ae41a374753b3ba83013e3
Vikky <vigvaithi>
2018-05-04 17:23:03 +0530

    again the hotel and charity table with proper not null values

--- sqlhotel.py
+++ sqlhotel.py
@@ -4,20 +4,20 @@ print("Opened database successfully")
 conn.execute('''CREATE TABLE Hotel
         (HotelID INT PRIMARY KEY     NOT NULL,
          HotelName TEXT    NOT NULL,
-         HotelPhone TEXT     NOT NULL,
-         HotelMail TEXT,
-         HotelPassword TEXT,
-         HotelAddress TEXT);''')
+         HotelPhone TEXT,
+         HotelMail TEXT NOT NULL,
+         HotelPassword TEXT NOT NULL,
+         HotelAddress TEXT NOT NULL);''')
 print("Table created successfully");
 
 print("Opened database successfully")
 conn.execute('''CREATE TABLE Charity
         (CharityID INT PRIMARY KEY     NOT NULL,
          CharityName TEXT    NOT NULL,
-         CharityPhone TEXT     NOT NULL,
-         CharityMail TEXT,
-         CharityPassword TEXT,
-         CharityAddress TEXT);''')
+         CharityPhone TEXT ,
+         CharityMail TEXT  NOT NULL,
+         CharityPassword TEXT  NOT NULL,
+         CharityAddress TEXT  NOT NULL);''')
 print("Table created successfully");
 
 conn.close()
