3e7532b1c154c3964a425ff4d180992711d1b8c0
Vikky <vigvaithi>
2018-05-05 21:42:41 +0530

    again the hotel and charity table with id autoincrement

--- sqlhotel.py
+++ sqlhotel.py
@@ -2,20 +2,20 @@ import sqlite3
 conn = sqlite3.connect('acms.db')
 print("Opened database successfully")
 conn.execute('''CREATE TABLE Hotel
-        (HotelID INT PRIMARY KEY     NOT NULL,
+        (HotelID INTEGER PRIMARY KEY AUTOINCREMENT,
          HotelName TEXT    NOT NULL,
          HotelPhone TEXT,
-         HotelMail TEXT NOT NULL,
+         HotelMail TEXT NOT NULL UNIQUE,
          HotelPassword TEXT NOT NULL,
          HotelAddress TEXT NOT NULL);''')
 print("Table created successfully");
 
 print("Opened database successfully")
 conn.execute('''CREATE TABLE Charity
-        (CharityID INT PRIMARY KEY     NOT NULL,
+        (CharityID INTEGER PRIMARY KEY AUTOINCREMENT,
          CharityName TEXT    NOT NULL,
          CharityPhone TEXT ,
-         CharityMail TEXT  NOT NULL,
+         CharityMail TEXT  NOT NULL UNIQUE ,
          CharityPassword TEXT  NOT NULL,
          CharityAddress TEXT  NOT NULL);''')
 print("Table created successfully");
