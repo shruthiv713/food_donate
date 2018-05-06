ebbd72ca05830ae10079f90bfd6c42a49389ad7e
Vikky <vigvaithi>
2018-05-06 18:51:14 +0530

    forget password with email

--- FoodDonate.py
+++ FoodDonate.py
@@ -1,5 +1,13 @@
 import sqlite3
 from flask import Flask ,render_template,request
+import smtplib
+from email.mime.text import MIMEText
+smtp_ssl_host = 'smtp.gmail.com' # smtp.mail.yahoo.com
+smtp_ssl_port = 465
+username = 'donatefood4'
+password = '@donatefood4'
+sender = 'donatefood4@gmail.com'
+
 app = Flask(__name__)
 @app.route('/')
 def mainfun():
@@ -9,6 +17,52 @@ def mainfun():
 @app.route('/signup')
 def signup():
     return render_template('Signup.html')
+
+@app.route('/forgot')
+def forgot():
+    return render_template('ForgotPassword.html')
+
+@app.route('/seemail', methods=['POST', 'GET'])
+def seemail():
+    if request.method == 'POST':
+        try:
+            mail = request.form['mail']
+            category = request.form['category']
+            with sqlite3.connect("acms.db") as con:
+                cur = con.cursor()
+                if category == 'hotel':
+                    cur.execute("SELECT HotelPassword from Hotel WHERE HotelMail='{}'".format(mail))
+                    hp = cur.fetchone();
+                    targets = [mail]
+                    msg = MIMEText('Your Food Donate Password Is:'+ hp[0])
+                    msg['Subject'] = 'Your Food Donate Password'
+                    msg['From'] = sender
+                    msg['To'] = ', '.join(targets)
+                    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+                    server.login(username, password)
+                    server.sendmail(sender, targets, msg.as_string())
+                    server.quit()
+                    msgDeatils = "Please Check Your Mail For The Password"
+                if category == 'orphanage':
+                    cur.execute("SELECT CharityPassword from Charity WHERE CharityMail='{}'".format(mail))
+                    hp = cur.fetchone();
+                    targets = [mail]
+                    msg = MIMEText('Your Food Donate Password Is:'+ hp[0])
+                    msg['Subject'] = 'Your Food Donate Password'
+                    msg['From'] = sender
+                    msg['To'] = ', '.join(targets)
+                    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+                    server.login(username, password)
+                    server.sendmail(sender, targets, msg.as_string())
+                    server.quit()
+                    msgDeatils = "Please Check Your Mail For The Password"
+                con.commit()
+        except:
+            msgDeatils = "Email Does not exists in the database "
+            con.rollback()
+        finally:
+            return render_template("ForgotPasswordSuccess.html",msgDeatils=msgDeatils)
+            con.close()
 
 
 @app.route('/addentry', methods=['POST', 'GET'])
